b1703a230e023c0d7bc7498237c37c9734bc5dd1
vlflex <lecsinvlad@mail.ru>
2023-10-05 23:22:51 +0300

    main_diagonal_mult

--- 11.py
+++ 11.py
@@ -66,6 +66,25 @@ class Matrix:
                 extracted_nums.append(self[i][j])
             return reduce(MULTIPLICATION, extracted_nums)
     
+    def main_diagonal_mult(self, index_i, index_j, nums_count = 4):
+        if index_i < 0 or index_j < 0:
+            raise MatrixIndexException(self.__height, self.__width, 0)
+        elif index_i >= self.__height:
+            raise MatrixIndexException(self.__height, self.__width, 1)
+        elif index_j >= self.__width:
+            raise MatrixIndexException(self.__height, self.__width, 2)
+        elif index_i + nums_count > self.__height:
+            raise MatrixIndexException(self.__height, self.__width, 3)
+        elif index_j + nums_count > self.__width:
+            raise MatrixIndexException(self.__height, self.__width, 4)
+        else:
+            extracted_nums = []
+            for k in range(nums_count):
+                i = index_i + k
+                j = index_j + k
+                extracted_nums.append(self[i][j])
+            return reduce(MULTIPLICATION, extracted_nums)
+    
     def __getitem__(self, index):
         return self.__data[index]
     
