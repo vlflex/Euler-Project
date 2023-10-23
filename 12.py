1e2d2e8f90cbee82a00126a959eebc2091d89ea2
vlflex <lecsinvlad@mail.ru>
2023-10-23 17:01:23 +0300

    get dividers count

--- 12.py
+++ 12.py
@@ -8,3 +8,6 @@ def get_dividers(num):
     for i in range(1, num + 1):
         if(num % i == 0):
             yield i
+            
+def get_dividers_count(num):
+    return len(list(get_dividers(num)))
