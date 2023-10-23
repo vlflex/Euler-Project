a8b7c848578e4f4a66b9a3c7bc47331fda740dec
vlflex <lecsinvlad@mail.ru>
2023-10-23 17:40:07 +0300

    dividers functiouns

--- 12.py
+++ 12.py
@@ -1,15 +1,10 @@
-from 3 import 
-
 def get_triangle_num(n):
-    num = 0
-    for i in range(1, n + 1):
-        num += i
-    return num
+    return sum(range(1, n + 1))
     
 def get_dividers(num):
+    dividers = []
     for i in range(1, num + 1):
         if(num % i == 0):
-            
             yield i
             
 def get_dividers_count(num):
