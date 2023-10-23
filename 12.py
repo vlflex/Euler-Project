3866d6aa07630ba893f35aad4d71a3e9965ab54d
vlflex <lecsinvlad@mail.ru>
2023-10-23 17:21:15 +0300

    rename 3

--- 12.py
+++ 12.py
@@ -1,3 +1,5 @@
+from 3 import 
+
 def get_triangle_num(n):
     num = 0
     for i in range(1, n + 1):
@@ -7,7 +9,30 @@ def get_triangle_num(n):
 def get_dividers(num):
     for i in range(1, num + 1):
         if(num % i == 0):
+            
             yield i
             
 def get_dividers_count(num):
     return len(list(get_dividers(num)))
+
+
+
+def main():
+    # count = 0
+    # i = 0
+    # while(count <= 500):
+    #     triangle_num = get_triangle_num(i)
+    #     count = get_dividers_count(triangle_num)
+    #     i += 1
+        
+    #     if(count == 20):
+    #         print(triangle_num)
+    #         break
+        
+    # else:
+    #     print(triangle_num)
+    
+    print(get_dividers_count(999999999))
+        
+if __name__ == '__main__':
+    main()
