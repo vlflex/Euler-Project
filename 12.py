f4f019b641bb70c877777e83f1fd4ef2127d351e
vlflex <lecsinvlad@mail.ru>
2023-10-23 17:00:45 +0300

    Get diviers

--- 12.py
+++ 12.py
@@ -3,3 +3,8 @@ def get_triangle_num(n):
         return 1
     else:
         return n + get_triangle_num(n - 1)
+    
+def get_dividers(num):
+    for i in range(1, num + 1):
+        if(num % i == 0):
+            yield i
