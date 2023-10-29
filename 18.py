406b870fd2c660b52ba35d3229b7ef9016f2325b
vlflex <lecsinvlad@mail.ru>
2023-10-29 22:56:45 +0300

    Get triangle sum method
    -Получает последовательность комманд (список из 0 и 1) где
    --0 - перемещение к левому нижнему числу
    --1 - перемещение к правому нижнему числу
    -Возвращает сумму чисел, которые будет встречены при выполнении команд

--- 18.py
+++ 18.py
@@ -34,8 +34,21 @@ class TriangleNum:
     
     def get_down_left(self, i, j):
         shift_left = lambda j: j if (j - 1 >= 0) else 0
-        return self.__value[i+1][shift_left(j-1)], (i + 1, shift_left(j-1))
+        return (self.__value[i+1][shift_left(j-1)],i + 1, shift_left(j-1))
     
     def get_down_right(self, i, j):
         shift_right = lambda j: j if (j + 1 > len(self.__value[i])) else len(self.__value[i]) - 1
         return (self.__value[i+1][shift_right(j+1)], i + 1, shift_right(j+1))
+
+def get_triangle_sum(commands, triangle_num):
+    i = j = 0
+    sum_ = triangle_num[0][0]
+    for option in commands:
+        if(option):
+            num, i, j = triangle_num.get_down_right(i, j)
+        else:
+            num, i, j = triangle_num.get_down_left(i, j)
+        sum_ += num  
+    else:
+        return sum_
+    
