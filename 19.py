61745ffde75ebd401d8cb2ebdf2834217ddbf089
vlflex <lecsinvlad@mail.ru>
2023-10-30 17:36:19 +0300

    get_delta method

--- 19.py
+++ 19.py
@@ -10,3 +10,16 @@ def is_leap_year(year):
         return True
     else:
         return False
+
+def get_delta(date):
+    if (date.month in [4, 6, 9, 11]):
+        delta = timedelta(days = 30)
+    elif (date.month == 2):
+        if(is_leap_year(date.year)):
+            delta = timedelta(days = 29)
+        else:
+            delta = timedelta(days = 28)
+    else:
+        delta = timedelta(days = 31)
+    return delta
+
