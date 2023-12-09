c11c8cc28847d143f36fcd383a4cbeadf0dca14a
vlflex <lecsinvlad@mail.ru>
2023-12-09 20:54:32 +0300

    delete others

--- 1.py
+++
@@ -1,9 +0,0 @@
-def numbers_kr_3_5(count):
-    return filter(lambda x: (x % 3 == 0) or (x % 5 == 0), range(1, count))
-            
-def main():
-    right_nums = list(numbers_kr_3_5(1000))
-    print(f'Ответ: {sum(right_nums)}')
-    
-if __name__ == '__main__':
-    main()
