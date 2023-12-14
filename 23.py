5255f4e560607cb126fe20af761849655d6c3ca0
vlflex <lecsinvlad@mail.ru>
2023-12-14 22:35:44 +0300

    Task23: bolean properties

--- 23.py
+++ 23.py
@@ -24,3 +24,30 @@ class Num:
                 if(i != 1):
                     yield self.__value // i
                 previous_i = i
+                
+    # булевое свойство "число - совершенное"
+    @property
+    def is_perfect(self) -> bool:
+        return self.value == sum(self.dividers)
+    
+    # булевое свойство "число - избыточное"
+    @property
+    def is_excess(self) -> bool:
+        return self.value < sum(self.dividers)
+    
+    # булевое свойство "число можно быть записано
+    # как сумма двух избыточных чисел"
+    @property 
+    def is_excess_sum(self) -> bool:
+        # число - чётное и его можно представить 
+        # как сумма двух (равных) избыточных чисел
+        LOWERST_EXCESS = 12
+        if(self.value % 2 == 0) and (Num(self.value//2).is_excess):
+            return True
+        # обычный поиск подходящих слагаемых
+        else:
+            for num1 in range(LOWERST_EXCESS, self.value // 2):
+                num2: int = self.value - num1
+                if(Num(num1).is_excess and Num(num2).is_excess):
+                    return True
+        return False
